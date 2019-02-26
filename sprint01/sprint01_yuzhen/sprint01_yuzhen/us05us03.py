0aa1cc8ea683f2954f15a3eee92f9541ae3de0eb
Yuzhen Zhang <42755788+ZYZMarshall@users.noreply.github.com>
2019-02-25 20:36:21 -0500

    Update us05us03.py

--- sprint01/sprint01_yuzhen/sprint01_yuzhen/us05us03.py
+++ sprint01/sprint01_yuzhen/sprint01_yuzhen/us05us03.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def check_birth_death(indi): #us03 and us05
+def check_birth_death(indi): #us03 
     res = True
 
     for i in indi:
@@ -10,7 +10,8 @@ def check_birth_death(indi): #us03 and us05
             #marri_date = datetime.strptime(indi[i]['MARR'],'%d%b%Y')
     
             if birthday > deathday:
-                warnin = 'ERROR death of individual %s comes before birth.' %i
+                warnin = ('ERROR:US03,%s:%s death date %d happened beofore birthdate %d'%\
+                         (indi[i]['DEAT_rec'],indi[i],indi[i]['DEAT'],indi['BIRT']))
                 print(warnin)
                 #file.write(warnin)
                 res = False
@@ -39,7 +40,7 @@ def marrige_before_death(ind,fam):
             else:
                 if marr_date > datetime.strptime(ind[hus_id]['DEAT'],"%d%b%Y"):
                     print('ERROR:US05,%s:%s death date %d happened beofore marrige %d'%\
-                         (fam[f]['MARR_REC'],ind[hus_id],ind[hus_id]['DEAT'],ind[hus_id]['MARR']))
+                         (fam[f]['MARR_rec'],ind[hus_id],ind[hus_id]['DEAT'],ind[hus_id]['MARR']))
                     res=False 
         if wife_id in ind:
             if 'DEAT' not in ind[wife_id]:
@@ -47,6 +48,6 @@ def marrige_before_death(ind,fam):
             else:
                 if marr_date > datetime.strptime(ind[wife_id]['DEAT'],"%d%b%Y"):
                     print('ERROR:US05,%s:%s death date %d happened beofore marrige %d'%\
-                         (fam[f]['MARR_REC'],ind[wife_id],ind[wife_id]['DEAT'],ind[wife_id]['MARR']))
+                         (fam[f]['MARR_rec'],ind[wife_id],ind[wife_id]['DEAT'],ind[wife_id]['MARR']))
                     res=False
     return res
