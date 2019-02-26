43be778311eafc15d47cdac27194f4be7fa0fa63
dingtongwang0 <44823332+dingtongwang0@users.noreply.github.com>
2019-02-25 20:06:51 -0500

    Add files via upload

--- Sprint1-DingtongWang/us02.py
+++ Sprint1-DingtongWang/us02.py
@@ -1,19 +1,24 @@
 from datetime import datetime
 
 
-def birth_before_marriage(indi):  # us02
+def birth_before_marriage(ind, fam):  # us02
     res = True
-
-    for i in indi:
-        if "BIRT" in indi[i] and "MARR" in indi[i]:
-            birthday = datetime.strptime(indi[i]["BIRT"], '%d%b%Y')
-            marriageday = datetime.strptime(indi[i]['MARR'], '%d%b%Y')
-
-            if birthday > marriageday:
-                warnin = 'ERROR marriage of individual %s comes before birth.' % i
-                print(warnin)
-                # file.write(warnin)
-                res = False
-
+    for f in fam:
+        hus_id = "0"
+        wife_id = "0"
+        if 'MARR' in fam[f] and 'HUSB' in fam[f] and 'WIFE' in fam[f]:
+            hus_id = fam[f]['HUSB']
+            wife_id = fam[f]['WIFE']
+            marr_date = datetime.strptime(fam[f]['MARR'],"%d%b%Y")
+        if hus_id in ind:
+            if marr_date < datetime.strptime(ind[hus_id]['BIRT'],"%d%b%Y"):
+                print('ERROR:US05,%s:%s birth date %d happened after marrige %d'%\
+                     (fam[f]['MARR_REC'],ind[hus_id],ind[hus_id]['BIRT'],ind[hus_id]['MARR']))
+                res=False
+        if wife_id in ind:
+            if marr_date < datetime.strptime(ind[wife_id]['BIRT'],"%d%b%Y"):
+                print('ERROR:US05,%s:%s birth date %d happened beofore marrige %d'%\
+                     (fam[f]['MARR_REC'],ind[wife_id],ind[wife_id]['BIRT'],ind[wife_id]['MARR']))
+                res=False
     return res
 
--- Sprint1-DingtongWang/us06.py
+++ Sprint1-DingtongWang/us06.py
@@ -1,19 +1,30 @@
 from datetime import datetime
 
 
-def divorce_before_death(indi):  # us06
+def divorce_before_death(ind,fam):
     res = True
-
-    for i in indi:
-        if "DIV" in indi[i] and "DEAT" in indi[i]:
-            divorceday = datetime.strptime(indi[i]["DIV"], '%d%b%Y')
-            deathday = datetime.strptime(indi[i]['DEAT'], '%d%b%Y')
-
-            if divorceday > deathday:
-                warnin = 'ERROR death of individual %s comes before divorcing.' % i
-                print(warnin)
-                # file.write(warnin)
-                res = False
-
+    for f in fam:
+        hus_id = "0"
+        wife_id = "0"
+        if 'DIV' in fam[f] and 'HUSB' in fam[f] and 'WIFE' in fam[f]:
+            hus_id = fam[f]['HUSB']
+            wife_id = fam[f]['WIFE']
+            divorce_date = datetime.strptime(fam[f]['DIV'],"%d%b%Y")
+        if hus_id in ind:
+            if 'DEAT' not in ind[hus_id]:
+                print('%s is alive and divorced!\n'%hus_id)
+            else:
+                if divorce_date > datetime.strptime(ind[hus_id]['DEAT'],"%d%b%Y"):
+                    print('ERROR:US05,%s:%s death date %d happened beofore divorce %d'%\
+                         (fam[f]['DIV_REC'],ind[hus_id],ind[hus_id]['DEAT'],ind[hus_id]['DIV']))
+                    res=False
+        if wife_id in ind:
+            if 'DEAT' not in ind[wife_id]:
+                print('%s is alive and divorced!\n'%wife_id)
+            else:
+                if divorce_date > datetime.strptime(ind[wife_id]['DEAT'],"%d%b%Y"):
+                    print('ERROR:US05,%s:%s death date %d happened beofore divorce %d'%\
+                         (fam[f]['DIV_REC'],ind[wife_id],ind[wife_id]['DEAT'],ind[wife_id]['DIV']))
+                    res=False
     return res
 
