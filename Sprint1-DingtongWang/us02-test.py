fcf567bc63587d0160f5fda5c5e1a7f079659396
dingtongwang0 <44823332+dingtongwang0@users.noreply.github.com>
2019-03-02 13:39:08 -0500

    Add files via upload

--- Sprint1-DingtongWang/us02-test.py
+++ Sprint1-DingtongWang/us02-test.py
@@ -2,54 +2,57 @@ import unittest
 from us02 import birth_before_marriage as us02
 
 
-ind1 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'sex': 'M', 'family': 'F23',
-				'BIRT': '31DEC2013', 'MARR': '31DEC2014'},
-		'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23SEP1960', 'sex': 'F', 'family': 'F23',
-				'BIRT': '31JAN2013', 'MARR': '31DEC2013'},
-		'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13FEB1999','sex': 'M', 'family': 'F23',
-				'BIRT': '30DEC2013', 'MARR': '31DEC2013'}}
+fam1 = {'F23':
+	{'fam': 'F23', 'MARR': '14FEB1880', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30'],'MARR_rec': '1'}}
+ind1 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15JUL1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31DEC2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23SEP1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'}}
 
-ind2 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'sex': 'M', 'family': 'F23',
-				'BIRT': '31DEC2013', 'MARR': '31DEC2000'},
-		'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'sex': 'F', 'family': 'F23',
-				'BIRT': '31JAN2013', 'MARR': '31DEC2013'},
-		'I19': {'id': 'I19', 'name': 'Dick /Smith/','sex': 'M', 'family': 'F23',
-				'BIRT': '30DEC2013', 'MARR': '31DEC2013'}}
+fam2 = {'F23':
+	{'fam': 'F23', 'MARR': '14FEB1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30'],'MARR_rec': '1'}}
+ind2 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15JUL1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31DEC2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23SEP1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'}}
 
-ind3 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'sex': 'M', 'family': 'F23',
-				'BIRT': '31DEC2013', 'MARR': '31DEC2000'},
-		'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'sex': 'F', 'family': 'F23',
-				'BIRT': '30NOV2013', 'MARR': '30JAN2013'},
-		'I19': {'id': 'I19', 'name': 'Dick /Smith/','sex': 'M', 'family': 'F23',
-				'BIRT': '30DEC2013', 'MARR': '1DEC2013'}}
+fam3 = {'F23':
+	{'fam': 'F23', 'MARR': '14FEB1980', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30'],'MARR_rec': '1'}}
+ind3 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'sex': 'M', 'family': 'F23', 'DEAT': '31DEC2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'}}
 
-ind4 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'sex': 'M', 'family': 'F23',
-				'MARR': '31DEC2000'},
-		'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'sex': 'F', 'family': 'F23',
-				'MARR': '30JAN2013'},
-		'I19': {'id': 'I19', 'name': 'Dick /Smith/','sex': 'M', 'family': 'F23',
-				'MARR': '1DEC2013'}}
-
-ind5 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'sex': 'M', 'family': 'F23',
-				'BIRT': '31DEC2013'},
-		'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'sex': 'F', 'family': 'F23',
-				'BIRT': '30NOV2013'},
-		'I19': {'id': 'I19', 'name': 'Dick /Smith/','sex': 'M', 'family': 'F23',
-				'BIRT': '30DEC2013'}}
-
+fam4 = {'F23':
+	{'fam': 'F23', 'HUSB': 'I01', 'WIFE': 'I07', 'CHIL': ['I19', 'I26', 'I30'],'MARR_rec': '1'}}
+ind4 = {'I01': {'id': 'I01', 'name': 'Joe /Smith/', 'BIRT': '15JUL1960', 'sex': 'M', 'family': 'F23', 'DEAT': '31DEC2013'},
+ 'I07': {'id': 'I07', 'name': 'Jennifer /Smith/', 'BIRT': '23SEP1960', 'sex': 'F', 'family': 'F23'},
+ 'I19': {'id': 'I19', 'name': 'Dick /Smith/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I26': {'id': 'I26', 'name': 'Jane /Smith/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I30': {'id': 'I30', 'name': 'Mary /Test/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'},
+  'I32': {'id': 'I32', 'name': 'Nick /Tary/', 'BIRT': '13FEB1981', 'sex': 'M', 'family': 'F23'},
+  'I44': {'id': 'I44', 'name': 'Cersi /Lanister/', 'BIRT': '13FEB1981', 'sex': 'F', 'family': 'F23'}}
 
 
 class Test_US02(unittest.TestCase):
-    def birth_before_marriage(self):
-        self.assertFalse(us02(ind1))
-        self.assertFalse(us02(ind2))
-        self.assertTrue(us02(ind3))
+    def test_birth_before_marriage(self):
+        self.assertFalse(us02(ind1, fam1))
+        self.assertTrue(us02(ind2, fam2))
 
-    def no_birth(self):
-        self.assertTrue(ind4)
+    def test_no_birth(self):
+        self.assertTrue(us02(ind3, fam3))
 
-    def no_marriage(self):
-        self.assertTrue(ind5)
+    def test_no_marriage(self):
+        self.assertTrue(us02(ind4, fam4))
 
 
 if __name__ == '__main__':
--- Sprint1-DingtongWang/us06.py
+++ Sprint1-DingtongWang/us06.py
@@ -6,25 +6,29 @@ def divorce_before_death(ind,fam):
     for f in fam:
         hus_id = "0"
         wife_id = "0"
+
         if 'DIV' in fam[f] and 'HUSB' in fam[f] and 'WIFE' in fam[f]:
             hus_id = fam[f]['HUSB']
             wife_id = fam[f]['WIFE']
-            divorce_date = datetime.strptime(fam[f]['DIV'],"%d%b%Y")
-        if hus_id in ind:
-            if 'DEAT' not in ind[hus_id]:
-                print('%s is alive and divorced!\n'%hus_id)
-            else:
-                if divorce_date > datetime.strptime(ind[hus_id]['DEAT'],"%d%b%Y"):
-                    print('ERROR:US06,%s:%s death date %d happened beofore divorce %d'%\
-                         (fam[f]['DIV_REC'],ind[hus_id],ind[hus_id]['DEAT'],ind[hus_id]['DIV']))
-                    res=False
-        if wife_id in ind:
-            if 'DEAT' not in ind[wife_id]:
-                print('%s is alive and divorced!\n'%wife_id)
-            else:
-                if divorce_date > datetime.strptime(ind[wife_id]['DEAT'],"%d%b%Y"):
-                    print('ERROR:US06,%s:%s death date %d happened beofore divorce %d'%\
-                         (fam[f]['DIV_REC'],ind[wife_id],ind[wife_id]['DEAT'],ind[wife_id]['DIV']))
-                    res=False
+            div_date = datetime.strptime(fam[f]['DIV'],"%d%b%Y")
+
+            if hus_id in ind:
+                if 'DEAT' not in ind[hus_id]:
+                    res = True
+                else:
+                    if div_date > datetime.strptime(ind[hus_id]['DEAT'],"%d%b%Y"):
+                        print('ERROR:US06,%s:%s divorce date %s happened after death %s'%\
+                             (fam[f]['DEAT_rec'],ind[hus_id],ind[hus_id]['BIRT'],ind[hus_id]['DEAT']))
+                        res = False
+
+            if wife_id in ind:
+                if 'DEAT' not in ind[wife_id]:
+                    res = True
+                else:
+                    if div_date > datetime.strptime(ind[wife_id]['DEAT'],"%d%b%Y"):
+                        print('ERROR:US06,%s:%s divorce date %s happened after death %s'%\
+                             (fam[f]['DEAT_rec'],ind[wife_id],ind[wife_id]['BIRT'],ind[wife_id]['DEAT']))
+                        res = False
+
     return res
 
