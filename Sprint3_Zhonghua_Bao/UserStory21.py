cbc4140d27647c295cb5d65b08105e97469334a2
Rosovski <44822333+Rosovski@users.noreply.github.com>
2019-04-06 20:10:14 -0400

    Add files via upload
    add annotation, forgot it lol

--- Sprint3_Zhonghua_Bao/UserStory21.py
+++ Sprint3_Zhonghua_Bao/UserStory21.py
@@ -1,19 +1,23 @@
 def userStory21(result):
+    """define a function to probe the wrong gender for the role in the family,
+       print out any anamoly statement,
+       store the problematic person in a list
+    """
     indi,fam = result['indi'],result['fam']
     anamoly = list()
 
     for key in fam.keys():
-        check_husb = fam[key]['HUSB']
-        check_wife = fam[key]['WIFE']
+        check_husb = fam[key]['HUSB']   # check the husband in the family
+        check_wife = fam[key]['WIFE']   # check the wife in the family
         
-        if indi[check_husb]['sex'] == 'F':
+        if indi[check_husb]['sex'] == 'F':   # find the problematic father
             if check_husb in anamoly:
                 continue
             else:
                 anamoly.append(check_husb)
                 print("ERROR: INDIVIDUAL: US21:", check_husb, "should be a male.")
         
-        if indi[check_wife]['sex'] == 'M':
+        if indi[check_wife]['sex'] == 'M':   # find the problematic mother
             if check_wife in anamoly:
                 continue
             else:
--- Sprint3_Zhonghua_Bao/UserStory32.py
+++ Sprint3_Zhonghua_Bao/UserStory32.py
@@ -1,13 +1,20 @@
 def userStory32(result):
+    """define a function to probe the multiple birth in the family,
+       print out any anamoly statement,
+       store the problematic family in a list
+    """
     indi,fam = result['indi'],result['fam']
     anamoly = list()
 
     for key in fam.keys():
         tmp = set()
-        if 'CHIL' in fam[key]:
+        
+        if 'CHIL' in fam[key]:   # only read the family has children
+            
             for person in fam[key]['CHIL']:
                 birth = indi[person]['BIRT']
-                if birth in tmp:
+                
+                if birth in tmp:   # find out the family whose kid has the same birthday
                     print("ANAMOLY: FAMILY: US32:",fam[key]['fam'],"has multiple birth")
                     anamoly.append(fam[key]['fam'])
                     break
