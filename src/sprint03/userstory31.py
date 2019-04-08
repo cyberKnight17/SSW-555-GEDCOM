0c5d870525fefab572e76ab74c6c2ba5f6b952c4
woshitc8535 <47222350+woshitc8535@users.noreply.github.com>
2019-04-07 23:04:51 -0400

    Update userstory31.py

--- src/sprint03/userstory31.py
+++ src/sprint03/userstory31.py
@@ -1,21 +1,23 @@
-#list living single
-def us31(fam,indi):
-    marriedlist=[]
-    allpeoplelist=[]
-    singlelist=[]
+# list living single
+def us31(fam, indi):
+    marriedlist = []
+    allpeoplelist = []
+    singlelist = []
     namelist = []
-    #print('person living and single')
+    # print('person living and single')
     for i in fam:
-        if 'DIV' not in fam[i] and 'WIFE' in fam[i] and 'HUSB' in fam[i] :
+        if 'DIV' not in fam[i] and 'WIFE' in fam[i] and 'HUSB' in fam[i]:
             marriedlist.append(fam[i]['HUSB'])
             marriedlist.append(fam[i]['WIFE'])
-    #get married couple id
+    # get married couple id
 
     for m in indi:
-        allpeoplelist.append(indi[m]['id'])
-        singlelist=[item for item in allpeoplelist if item not in marriedlist]
+        if 'DEAT' not in indi[m]:
+            allpeoplelist.append(indi[m]['id'])
+            singlelist = [item for item in allpeoplelist if item not in marriedlist]
     for x in singlelist:
-        print(f'US31:{indi[x]["id"]} is living and single.')
+        print(f'US31:{indi[x]["name"]} is living and single.')
         namelist.append(indi[x]['name'])
     return namelist
+
 
