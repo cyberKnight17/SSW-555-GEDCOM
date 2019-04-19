eca16741765a08ae507d39b31bfb01e9703476c5
woshitc8535 <47222350+woshitc8535@users.noreply.github.com>
2019-04-19 01:04:06 -0400

    Add files via upload

---
+++ user_story/userstory39.py
@@ -0,0 +1,42 @@
+# List upcoming anniversaries
+import datetime
+
+
+def us39(fam, indi):
+    # get married fam
+    huslist = []
+    wifelist = []
+    couple = []
+    for i in fam:
+        if 'DIV' not in fam[i] and 'MARR' in fam[i]:
+            marrydate = datetime.datetime.strptime(fam[i]['MARR'], '%d%b%Y')
+            # print(marrydate)
+            for x in range(30):
+                comingday = (datetime.datetime.now() + datetime.timedelta(days=x))
+                couplelist = []
+                if comingday.month == marrydate.month and comingday.day == marrydate.day:
+                    huslist.append(fam[i]['HUSB'])
+                    wifelist.append(fam[i]['WIFE'])
+                    couplelist.append(fam[i]['HUSB'])
+                    couplelist.append(fam[i]['WIFE'])
+                    couple.append(couplelist)
+
+
+    for m in couple:
+        for n in m:
+            if 'DEAT' in indi[n]:
+                couple.remove(m)
+
+
+    for v in couple:
+        coupleidname=[]
+        for c in v:
+            name = indi[c]['name']
+            id= indi[c]['id']
+            idname=id+' '+name
+            coupleidname.append(idname)
+        print('US31:',' '.join(coupleidname),'are living couple whose marriage anniversaries occur in the next 30 days')
+
+
+
+
